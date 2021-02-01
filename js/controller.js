7ab2645ef386635f4a14b53d97d2b637afe135cd
noyhason3 <noyhason3@gmail.com>
2021-02-01 16:12:05 +0200

    renderPortfolio() added

---
+++ js/controller.js
@@ -0,0 +1,44 @@
+var gProjs = [
+    {
+        id: 'sokoban',
+        name: 'Sokoban',
+        title: 'Better push those boxes',
+        desc: 'lorem ipsum lorem ipsum lorem ipsum',
+        url: 'projs/sokoban',
+        publishedAt: 1448693940000,
+        labels: ['Matrixes', 'keyboard events'],
+      },
+      {
+          id: 'minesweeper',
+          name: 'Minesweeper',
+          title: 'Improved Minesweeper ',
+          desc: 'lorem ipsum lorem ipsum lorem ipsum',
+          url: 'projs/sokoban',
+          publishedAt: 3243234332,
+          labels: ['Matrixes', 'Mines'],
+        },     {
+            id: 'sokoban',
+            name: 'Sokoban',
+            title: 'Better push those boxes',
+            desc: 'lorem ipsum lorem ipsum lorem ipsum',
+            url: 'projs/sokoban',
+            publishedAt: 1448693940000,
+            labels: ['Matrixes', 'keyboard events'],
+          },
+          {
+              id: 'minesweeper',
+              name: 'Minesweeper',
+              title: 'Improved Minesweeper ',
+              desc: 'lorem ipsum lorem ipsum lorem ipsum',
+              url: 'projs/sokoban',
+              publishedAt: 3243234332,
+              labels: ['Matrixes', 'Mines'],
+            },
+    ];
+
+
+function getProjs(){
+    return gProjs;
+}
+
+
---
+++ js/service.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function onInit(){
+    renderPortfolio();
+}
+
+function renderPortfolio(){
+    var projs = getProjs();
+    var strHtml = ''
+
+    for (var i= 0; i<projs.length;i++){
+        strHtml += `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${i+1}">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/portfolio/0${i+1}-thumbnail.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${projs[i].name}</h4>
+          <p class="text-muted">${projs[i].labels}</p>
+        </div>
+      </div>`
+    }
+    var $elPortfolios = $('.portfolios')
+    $elPortfolios.html(strHtml)
+
+}
